84c3869c5d4fddb287f5423a5f506d284273d350
Iñaki Paramo <61165846+iparamob@users.noreply.github.com>
2020-05-25 08:02:46 +0200

    Update downloadVideo.py

--- downloadVideo.py
+++ downloadVideo.py
@@ -1,4 +1,4 @@
-# importing module 
+# importing module https://github.com/ytdl-org/youtube-dl
 import youtube_dl 
 
 ydl_opts = {} 
@@ -7,7 +7,7 @@ def dwl_vid():
 	with youtube_dl.YoutubeDL(ydl_opts) as ydl: 
 		ydl.download([zxt]) 
 
-link_of_the_video = "https://www.youtube.com/watch?v=_A_tdAwBJgk"
+link_of_the_video = "https://www.youtube.com/watch?v=XXXXXX"
 zxt = link_of_the_video.strip() 
 
 dwl_vid() 
